48514d415d04a258b9dc5f3cb367ecac9220c719
MRyabchikov <m.ryabchikov@yandex.ru>
2024-10-24 16:52:35 +0300

    Made all atmosphere models compilable

--- lib/layered_model.h
+++ lib/layered_model.h
@@ -1,25 +1,17 @@
 #pragma once
+#include "atmosphere_model.h"
 #include <utility>
 #include <vector>
 
 class LayeredAtmosphere : public AtmosphereModel {
 public:
-    LayeredAtmosphere(const std::vector<AtmospherModel> atmospheres,
-                      const std::vector<double> _heights) :
-        models{atmospheres}, heights{_segments} {
-        if(models.size() == 0 || heights.size() == 0)
-            throw std::runtime_error("Empty vector provided");
-        if (heights.size() - 1 != models.size())
-            throw std::runtime_error("Invalid sizes of arguments");
-        for (size_t i = 0; i < heights.size() - 1; ++i)
-            if (heights[i] >= heights[i + 1])
-                throw std::runtime_error("The monotony of heights is broken");
-    }
+    LayeredAtmosphere(const std::vector<AtmosphereModel*> atmospheres,
+                      const std::vector<double> _heights);
 
     virtual double N(double h) override;
 
 private:
     // heights - [m]
     std::vector<double> heights;
-    const std::vector<AtmosphereModel> models;
+    const std::vector<AtmosphereModel*> models;
 };
