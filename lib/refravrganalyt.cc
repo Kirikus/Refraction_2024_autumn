1d68f3ae60693a7db94c09974de66e9f96bbf013
Kirikus <kirikusan@gmail.com>
2024-12-16 20:56:04 +0300

    fix: refraction model using atmosperic models
    correct member fields used + using exp instead of pow.

--- lib/refravrganalyt.cc
+++ lib/refravrganalyt.cc
@@ -1,30 +1,28 @@
 #include "refravrganalyt.h"
 
-double AverageKAnalytical::k(double h_a, double h_s, double R)
-{
-    double oRe = get_Re();
-    double delta_h = h_a - h_s;
-    // 'cos_psi' calculated as in GeometricLine
-    // using Pythagorean theorem
-    double cos_psi = (R*R - delta_h*delta_h)/R;
-    double varNH = 1e-6*atmo->N_s*cos_psi*oRe/atmo->H_b;
-    double varLN = std::log((std::pow(delta_h/atmo->H_b) - varNH)/(1 - varNH));
+double AverageKAnalytical::k(double h_a, double h_s, double R) {
+  double oRe = get_Re();
+  double delta_h = h_a - h_s;
+  // 'cos_psi' calculated as in GeometricLine
+  // using Pythagorean theorem
+  double cos_psi = (R * R - delta_h * delta_h) / R;
+  double varNH = 1e-6 * atmo->Ns * cos_psi * oRe / atmo->Hb;
+  double varLN = std::log((std::exp(delta_h / atmo->Hb) - varNH) / (1 - varNH));
 
-    double k_avg = (atmo->H_b/delta_h)*varLN;
-    return k_avg;
+  double k_avg = (atmo->Hb / delta_h) * varLN;
+  return k_avg;
 }
 
+double AveragePAnalytical::k(double h_a, double h_s, double R) {
+  double oRe = get_Re();
+  double delta_h = h_a - h_s;
+  // 'cos_psi' calculated as in GeometricLine
+  // using Pythagorean theorem
+  double cos_psi = (R * R - delta_h * delta_h) / R;
+  double varNH = 1e-6 * atmo->Ns * cos_psi * oRe / atmo->Hb;
 
-double AveragePAnalytical::k(double h_a, double h_s, double R)
-{
-    double oRe = get_Re();
-    double delta_h = h_a - h_s;
-    // 'cos_psi' calculated as in GeometricLine
-    // using Pythagorean theorem
-    double cos_psi = (R*R - delta_h*delta_h)/R;
-    double varNH = 1e-6*atmo->N_s*cos_psi*oRe/atmo->H_b;
-
-    double rho_avg = (1/varNH)*(std::pow(delta_h/atmo->H_b) - 1)/(delta_h/atmo->H_b);
-    double k_avg = rho_avg/(rho_avg - oRe);
-    return k_avg;
+  double rho_avg =
+      (1 / varNH) * (std::exp(delta_h / atmo->Hb) - 1) / (delta_h / atmo->Hb);
+  double k_avg = rho_avg / (rho_avg - oRe);
+  return k_avg;
 }
