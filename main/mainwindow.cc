980def50bac3cf49ae20570898df691521880e9b
Mox <mox.dev@yandex.ru>
2024-12-21 17:38:58 +0300

    Fixed program crash. Graphs get cleared before next replot.

--- main/mainwindow.cc
+++ main/mainwindow.cc
@@ -12,28 +12,23 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow() { delete ui; }
 
 void MainWindow::redrawGraph() {
-    // std::cout<<"Inside of redraw\n";
     auto direct_refr_model = ui->direct->getRefrModel();
     auto inverse_refr_model = ui->inverse->getRefrModel();
-    // std::cout<<"Got refr models\n";
 
     QVector<double> direct_refr_vals = ui->direct->getRefrValues();
     QVector<double> inverse_refr_vals = ui->inverse->getRefrValues();
-    // std::cout<<"Got refr vals\n";
     double dir_h_a = direct_refr_vals[0], dir_h_s = direct_refr_vals[1], dir_R = direct_refr_vals[2];
+    // some vars stay unused, then why even leave a TextEdit for them?
     double inv_h_a = inverse_refr_vals[0], inv_h_s = inverse_refr_vals[1], inv_R = inverse_refr_vals[2];
-    // std::cout<<"Wrote refr vals\n";
 
     QCustomPlot* gView = ui->graphView;
     gView->legend->setVisible(true);
-
+    gView->clearGraphs();
     RefrResult dir_ans = direct_refr_model->calculate(dir_h_a, dir_h_s, dir_R);
     InvRefrResult inv_ans = inverse_refr_model->reverse(inv_h_a, dir_ans.psi_d, inv_R);
 
     QPen pen;
     QVector<double> x, y;
-
-    gView->graph()->setScatterStyle(QCPScatterStyle::ssSquare);
 
     pen.setColor(QColor(0, 0, 0));
     //station
@@ -41,6 +36,7 @@ void MainWindow::redrawGraph() {
     y = {dir_h_a};
 
     gView->addGraph();
+    gView->graph()->setScatterStyle(QCPScatterStyle::ssSquare);
     gView->graph()->setData(x, y);
     gView->graph()->setPen(pen);
     gView->graph()->setName("Station");
@@ -50,8 +46,9 @@ void MainWindow::redrawGraph() {
     y = {dir_h_s};
 
     gView->addGraph();
+    gView->graph()->setScatterStyle(QCPScatterStyle::ssSquare);
     gView->graph()->setData(x, y);
-    pen.setColor(QColor(255, 0, 0));
+    pen.setColor(QColor(255, 0, 0, 126));
     gView->graph()->setPen(pen);
     gView->graph()->setName("Target-direct");
 
@@ -60,16 +57,15 @@ void MainWindow::redrawGraph() {
     y = {inv_ans.h_s};
 
     gView->addGraph();
+    gView->graph()->setScatterStyle(QCPScatterStyle::ssSquare);
     gView->graph()->setData(x, y);
-    pen.setColor(QColor(0, 0, 255));
+    pen.setColor(QColor(0, 0, 255, 126));
     gView->graph()->setPen(pen);
     gView->graph()->setName("Target-inverse");
 
 
-    gView->graph()->setScatterStyle(QCPScatterStyle::ssNone);
-
     // direct graph
-    pen.setColor(QColor(255, 0, 0));
+    pen.setColor(QColor(255, 0, 0, 126));
     // direct declination
     x = {0, dir_ans.d/5};
     y = {dir_h_a, dir_h_a - dir_ans.d/5*std::tan(dir_ans.psi_d)};
@@ -77,18 +73,19 @@ void MainWindow::redrawGraph() {
     gView->addGraph();
     gView->graph()->setData(x, y);
     gView->graph()->setPen(pen);
-    gView->graph()->setName("Direct");
+    gView->graph()->setName("Direct declination");
 
     // direct glide
     x = {dir_ans.d*4/5, dir_ans.d};
-    y = {dir_h_s, dir_h_s + dir_ans.d/5*std::tan(dir_ans.psi_g)};
+    y = {dir_h_s + dir_ans.d/5*std::tan(dir_ans.psi_g), dir_h_s};
 
     gView->addGraph();
     gView->graph()->setData(x, y);
     gView->graph()->setPen(pen);
+    gView->graph()->setName("Direct glide");
 
     // inverse graph
-    pen.setColor(QColor(0, 0, 255));
+    pen.setColor(QColor(0, 0, 255, 126));
     //inverse declination
     x = {0, inv_ans.d/5};
     y = {inv_h_a, inv_h_a - inv_ans.d/5*std::tan(dir_ans.psi_d)};
@@ -96,15 +93,18 @@ void MainWindow::redrawGraph() {
     gView->addGraph();
     gView->graph()->setData(x, y);
     gView->graph()->setPen(pen);
-    gView->graph()->setName("Inverse");
+    gView->graph()->setName("Inverse declination");
 
     //inverse glide
     x = {inv_ans.d*4/5, inv_ans.d};
-    y = {inv_ans.h_s, inv_ans.h_s + inv_ans.d/5*std::tan(inv_ans.psi_g)};
+    y = {inv_ans.h_s + inv_ans.d/5*std::tan(inv_ans.psi_g), inv_ans.h_s};
 
     gView->addGraph();
     gView->graph()->setData(x, y);
     gView->graph()->setPen(pen);
+    gView->graph()->setName("Inverse glide");
 
-    gView->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);// | QCP::iSelectPlottables);
+
+    gView->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
+    gView->replot();
 }
